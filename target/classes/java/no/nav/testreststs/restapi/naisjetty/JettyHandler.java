3e42134b1785d926295a85443d76ce67c048ef9e
Kveim <K134403@ADEO.NO>
2019-01-10 09:09:18 +0000

    tatt bort en del som ikke skulle være med

--- target/classes/java/no/nav/testreststs/restapi/naisjetty/JettyHandler.java
+++
@@ -1,8 +0,0 @@
-package no.nav.testreststs.restapi.naisjetty;
-
-public class JettyHandler {
-
-    public static final String HOST = "0.0.0.0";
-    public static final int PORT = 8080;
-    public static final int IDLE_TIMEOUT = 30000;
-}
